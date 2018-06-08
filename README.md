fa9205f8b0b5b67e2a4dc209343a5133c29329b4
mtongpalad <37819143+mtongpalad@users.noreply.github.com>
2018-06-07 21:00:11 -0700

    lol

---
+++ README.md
@@ -0,0 +1,2 @@
+# 3870fa17_longscroll
+long scroll project
---
+++ js/scroll.js
@@ -0,0 +1,71 @@
+var canvas = document.getElementById("canvas");
+var ball = document.getElementById("ball");
+function myFunction() {
+var y = document.getElementById().loop;}
+
+window.onscroll = function(){
+
+  var y = window.pageYOffset;
+
+  console.log(y);
+
+
+
+  ball.style.top = 12 + y * 1.0 + "px";
+
+   if (y > 0) {ball.style.left = "510px";}
+
+
+  if (y > 650) {ball.style.left = "500px";}
+  if (y > 700) {ball.style.left = "550px";}
+  if (y > 750) {ball.style.left = "600px";}
+  if (y > 800) {ball.style.left = "650px";}
+  if (y > 850) {ball.style.left = "700px";}
+  if (y > 900) {ball.style.left = "750px";}
+
+
+  if (y > 1100) {ball.style.visibility = "hidden";}
+  if (y > 2500) {ball.style.visibility = "visible";}
+  if (y > 2500) {ball.style.left = "510px";}
+
+
+
+
+
+
+  if (y > 3200) {ball.style.left = "510px";}
+  if (y > 3250) {ball.style.left = "550px";}
+  if (y > 3300) {ball.style.left = "600px";}
+  if (y > 3350) {ball.style.left= "650px";}
+  if (y > 3400) {ball.style.left = "750px";}
+
+
+
+  if (y > 3500) {ball.style.visibility = "hidden";}
+  if (y > 5000) {ball.style.visibility = "visible";}
+  if (y > 5000) {ball.style.left = "510px";}
+
+
+
+
+
+
+  if (y > 5750) {ball.style.left = "510px";}
+  if (y > 5800) {ball.style.left = "550px";}
+  if (y > 5850) {ball.style.left = "600px";}
+  if (y > 5900) {ball.style.left= "650px";}
+  if (y > 6950) {ball.style.left = "750px";}
+
+
+
+  if (y > 3500) {ball.style.visibility = "hidden";}
+  if (y > 5000) {ball.style.visibility = "visible";}
+  if (y > 5000) {ball.style.left = "510px";}
+
+
+
+
+
+
+
+};
